cd62b57b54a2d9d1dce6e76d4b790e39c799a952
JoaquinNeulist <joaquinneulist@gmail.com>
2024-04-15 20:52:08 -0300

    fin

--- js/details.js
+++ js/details.js
@@ -8,9 +8,9 @@ console.log(producto(data, id));
 let productoEncontrado = producto(data, id);
 
 detalleContainer.innerHTML = `
-<div class="flex flex-col md:flex-row items-center justify-center bg-gray-100 rounded-lg shadow-lg p-8 space-y-8 md:space-x-8">
+<div class="flex flex-col md:flex-row items-center justify-center rounded-lg shadow-lg p-8 space-y-8 md:space-x-8">
     <div class="md:w-1/2">
-        <h1 class="text-4xl font-bold mb-4">${productoEncontrado.modelo}</h1>
+        <h1 class="text-3xl font-bold mb-4">${productoEncontrado.modelo}</h1>
         <p class="text-lg mb-4">${productoEncontrado.descripcion}</p>
         <div class="flex justify-between items-center mb-4">
             <p class="text-lg font-bold">Precio:</p>
@@ -18,7 +18,7 @@ detalleContainer.innerHTML = `
         </div>
         <div class="flex justify-between items-center mb-4">
             <p class="text-lg font-bold">Descuento:</p>
-            <p class="text-lg">-$${productoEncontrado.descuento}</p>
+            <p class="text-lg">${productoEncontrado.descuento}%</p>
         </div>
         <div class="flex justify-between items-center mb-4">
             <p class="text-lg font-bold">Stock:</p>
@@ -29,15 +29,15 @@ detalleContainer.innerHTML = `
             <p class="text-lg">$${productoEncontrado.precio_total}</p>
         </div>
     </div>
-    <div class="md:w-1/2">
-        <img class="h-80 w-80 object-contain rounded-lg shadow-md" src="${productoEncontrado.imagen}" alt="">
-    </div>
-</div>
-<div class="flex justify-center mt-8">
-    <button id="boton" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg shadow-lg">
-        Agregar al carrito
-    </button>
-</div>
+    <div class="md:w-1/2 flex flex-end justify-end">
+        <img class="h-[240px] w-[240px] object-contain rounded-lg shadow-md md:h-[360px] w-[360px]" src="${productoEncontrado.imagen}" alt="">
+        </div>
+        </div>
+        <div class="w-full flex justify-center mb-4 md:justify-end items-end mr-4 mt-4 lg:mt-4 justify-end items-end">
+            <button id="boton" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg shadow-lg">
+                Agregar al carrito
+            </button>
+        </div>
 `;
 
 let boton = document.getElementById('boton');
