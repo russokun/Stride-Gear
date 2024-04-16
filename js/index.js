209c518faee22ae4da6440f71adb5f7e1c096982
russokun <agustin.russo2323@gmail.com>
2024-04-15 23:03:17 -0400

    carrusel CTA actualizado a 2 segs, se agrego carrusel con las marcas, un menu hamburguesa para resoluciones mas pequenas, y un boton de incentivo de compra.

--- js/index.js
+++ js/index.js
@@ -9,4 +9,8 @@ function nextItem() {
   currentItem = (currentItem + 1) % carouselItems.length;
   showItem(currentItem);
 }
-setInterval(nextItem, 4000); // Cambia la imagen cada 4 segundos (4000 milisegundos)
+setInterval(nextItem, 2000); // Cambia la imagen cada 2 segundos (2000 milisegundos)
+function Menu(e){
+  let list = document.querySelector('ul')
+  e.name === "menu" ? (e.name = "close",list.classList.add('top-[100px]') , list.classList.add('opacity-100')) :(e.name = "menu" ,list.classList.remove('top-[100px]'),list.classList.remove('opacity-100'))
+}
