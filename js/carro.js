56d059d9c8038e14f9dbb396da980feea868ad41
russokun <agustin.russo2323@gmail.com>
2024-04-17 03:13:43 -0400

    a medias vamos pero vamos

--- js/carro.js
+++ js/carro.js
@@ -20,7 +20,7 @@ function renderCarrito(array, contenedor) {
     <div class="flex flex-col   p-4 shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]  my-4 bg-white rounded-lg items-center ">
         <img class="w-[100px]" src="${obj.imagen}" alt="">
         <h2>${obj.marca} ${obj.modelo} </h2>
-        <h3> Precio: ${obj.precio}</h3>
+        <h3> Precio: $ ${obj.precio}</h3>
         <h3>Cantidad disponible: ${obj.stock}</h3>
         <button class='flex ' data-deleteCard='${obj.id}'>
         <img class='w-[30px]' src="./assests/img/cart_off_icon_135804.png" alt="" data-delete-card='${obj.id}'>
@@ -53,14 +53,74 @@ shoppingContainer.addEventListener("click", (e) => {
   location.reload();
 });
 
-/* function actualizarCantidad(input, precio, descuento) {
-  const cantidad = parseInt(input.value); // Obtenemos la cantidad actual del input y la convertimos a un número entero
-  // const precioDescuento = precio - (precio * (descuento / 100)); // Calculamos el precio con el descuento aplicado
-  const nuevoValor = precioDescuento * cantidad; // Calculamos el nuevo valor basado en la cantidad y el precio con descuento
 
-  // Actualizamos el valor del input con el nuevo valor
-  input.value = nuevoValor.toFixed(2); // Usamos toFixed(2) para redondear el nuevo valor a 2 decimales
-}
+
+
+function renderInput (producto,contenedor)  {
+    let template = document.createElement("template");
+    template=''
+    producto.forEach(producto =>{
+        template =`
+        <h2>${producto.marca} ${producto.modelo} ${producto.id}</h2>
+    <p data-precio='${producto.id}'> - $${producto.precio}  (Descuento del ${producto.descuento}%)</p>
+    <div class='flex flex-row items-center'>
+    <button class='p-3 ' data-boton-mas='${producto.cantidad}' data-id ='${producto.id}'>+</button>
+    
+    <input type="text" class="w-[20px]" name="" max='100' min='1' value='1' id="" data-cantidad='${producto.id}'>
+    
+    <button class='p-3 ' data-boton-menos='${producto.cantidad}' data-id ='${producto.id}'>-</button>
+    </div>
+  `
+  contenedor.innerHTML += template
+})
+
+};
+renderInput(articulos, listaProductos,);
+//BOOTONES
+let numeroCantidadSuma = 1
+listaProductos.addEventListener('click', e => {
+    
+
+        let mas = e.target.dataset.botonMas;
+        let menos = e.target.dataset.botonMenos;
+        
+        
+        if (mas) {
+            let idCapturado = e.target.dataset.id
+            console.log(idCapturado);
+            let cantidadd = document.querySelector(`input[data-cantidad="${idCapturado}"]`)
+            //console.log(parseInt(cantidadd.value));
+            cantidadd.value = parseInt(cantidadd.value )+ 1
+            console.log(cantidadd.value);
+        }
+        
+        if (menos) {
+            let idCapturado = e.target.dataset.id
+            console.log(idCapturado);
+            let cantidadd = document.querySelector(`input[data-cantidad="${idCapturado}"]`)
+            //console.log(parseInt(cantidadd.value));
+            cantidadd.value = parseInt(cantidadd.value )- 1
+            console.log(cantidadd.value);
+        }
+    
+});
+console.log(numeroCantidadSuma)
+
+//-------------------------------------------------------------------------------
+/* let cantidad = document.querySelector('p[data-cantidad="${producto.id}"]')
+console.log(cantidad); */
+let multiplicarPorPrecio= precio =>
+    numeroCantidadSuma * precio;
+
+/* let precio = ar
+let total= multiplicarPorPrecio()
+
+// Ejemplo de uso:
+let precioUnitario = articulos.filter(precio =>  ) // Ejemplo de precio
+let total = multiplicarPorPrecio(precioUnitario);
+console.log(total) */; // Mostrará el total multiplicado por el precio
+
+
 //funcion para calcular el precio con descuento
 let calcularPrecionConDescuento = (precio, descuento) =>
   precio - precio * (descuento / 100);
@@ -77,72 +137,27 @@ function agregarProducto( precio, descuento) {
 
   actualizarCarrito();
 }
-let incrementarContador = () => contadorProductos++;
 
+let incrementarContador = () => contadorProductos++;
 
 let sumarProducto = (precio) =>
   (precioProductos += precio);
 
 //funcion para calcular desc total
 let calcularDescuentoTotal = (precio, descuento) =>
-  (descuentoTotal += (precio * descuento) / 100); */
-//funcion para renderizar input
+  (descuentoTotal += (precio * descuento) / 100); 
 
-function renderInput (producto,contenedor)  {
-    let template = document.createElement("template");
-    template=''
-    producto.forEach(producto =>{
-        template =`
-        <h2>${producto.marca} ${producto.modelo} ${producto.id}</h2>
-        
-     
-     <p> - $${producto.precio}  (Descuento del ${producto.descuento}%)</p>
-  <button data-boton-mas='${producto.cantidad}'>+</button>
-  <label>Cantidad:
-      <input  class='border border-black w-[50px]' type="number" name="" max="${producto.stock}" min="1" value='1' onchange="actualizarCantidad(this, ${producto.precio}, ${producto.descuento})">
-  </label>
-  <button data-boton-menos='${producto.cantidad}'>-</button>
-  `
-  contenedor.innerHTML += template
-})
-
-};
-renderInput(articulos, listaProductos);
-//BOOTON MAS
- listaProductos.addEventListener('click', e=>{
-let mas = e.target.dataset.botonMas
-console.log(parseInt(mas));
-//localStorage.setitem('', mas)
-//cambiar el input por un <p> con un numero y a medida que se haga el click se sume el  numero
-}) 
-
-//BOTON MENOS
-listaProductos.addEventListener('click', e=>{
-    let menos =e.target.dataset.botonMenos    
-    console.log(parseInt(menos));
-    //localStorage.setitem('', mas)
-}) 
-
-
-/* 
 function actualizarCarrito() {
   let precioConDescuento = precioProductos - descuentoTotal;
 
-  document.getElementById("descuento").textContent = descuentoTotal.toFixed(2);
+  document.getElementById("descuento").textContent = 'hola'
   document.getElementById("contador-productos").textContent = contadorProductos;
-  document.getElementById("precio-productos").textContent =
-    precioProductos.toFixed(2);
-  document.getElementById("total-productos").textContent =
-    precioConDescuento.toFixed(2);
+  document.getElementById("precio-productos").textContent = precioProductos.toFixed(2);
+  document.getElementById("total-productos").textContent = precioConDescuento.toFixed(2);
 }
 
-// interpolar data.propiedad
 
-  articulos.forEach((producto) => {
-    producto.nombre = ` ${producto.marca} ${producto.modelo} ${producto.id} `;
-    agregarProducto(producto.nombre, producto.precio, producto.descuento);
-  }); */
 
 //[id,cantidad]
 //addeventlistener
-//reduce
+//reduce*/
