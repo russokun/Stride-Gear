23ebd3ba49a99eb1f65bbe8e3e3ecbd7a4d25cb5
JoaquinNeulist <joaquinneulist@gmail.com>
2024-04-23 17:33:26 -0300

    borde form, btn compra, nav, descuento, pago, $ en producto hecho

--- js/app.js
+++ js/app.js
@@ -18,58 +18,51 @@ let tipoDeIndumentaria = [... new Set(data.map(indumentaria => indumentaria.tipo
 
 //CREACION Y RENDERIZADO DE CARDS
 function renderCards(productos, contenedor) {
-    contenedor.innerHTML = ''
+    contenedor.innerHTML = '';
     if (productos.length === 0) {
         renderNoResultsMessage(contenedor);
         return;
     }
-    let template = document.createElement('template')
 
     productos.forEach(obj => {
-        template = `
-           <div class= 'flex flex-col  w-3/4 gap-3 p-3 bg-white rounded-lg md:w-1/3 lg:w-1/4 hover:scale-110 duration-700 mt-3  shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(45,78,255,0.15)]' >
-           
-           <img class=" h-1/2 object-contain " src=" ${obj.imagen}" alt="${obj.modelo}">
-           <h3 class= "font-medium pl-4 text-lg ">${obj.marca}</h3>
-           <h3 class= "font-medium pl-4 text-lg ">${obj.modelo}</h3>
-           
-           <p class="pl-4 pb-[10px]"> ${obj.descripcion.slice(0, 40)}...</p>
-           <p class="pl-4 pb-[10px] font-bold">$${obj.precio}</p>
-           <div class='flex flex-row justify-around'>
-
-           <a href="./detalles.html?id=${obj.id}" class= "bg-black text-white p-2 w-2/4 text-center flex justify-center items-center rounded-xl ">Detalles</a>
-           <button>
-           <img class='w-[50px]' src="" alt="">
-       </button> 
-
-                  
-           ${!productoPush.includes(obj.id) ?
-
-               `
-               <button>
-               <img class='w-[50px]' src="./assests/img/shopping-cart-verified-symbol_icon-icons.com_56123.png" alt="" data-compra ='${obj.id}'>
-               </button> 
-               `
-              : 
-               `
-               <button>
-               <img class='w-[50px]' src="./assests/img/shopping-cart-cancel-button_icon-icons.com_56130.png" alt="" data-compra ='${obj.id}'>
-               </button> 
-               `
+        let precioMostrar = obj.precio;
+        if (obj.descuento) {
+            // Si el producto tiene descuento, calcular el precio con descuento
+            const precioDescuento = obj.precio - (obj.precio * (obj.descuento / 100));
+            precioMostrar = `<del>${obj.precio}</del> $${precioDescuento}`;
         }
 
-           </div>       
-           </div>
-           `
-           contenedor.innerHTML += template
-
-        });
-            
+        let template = `
+            <div class='flex flex-col w-3/4 gap-3 p-3 bg-white rounded-lg md:w-1/3 lg:w-1/4 hover:scale-110 duration-700 mt-3 shadow-[-10px_-10px_30px_4px_rgba(0,0,0,0.1),_10px_10px_30px_4px_rgba(45,78,255,0.15)]'>
+                <img class="h-1/2 object-contain" src="${obj.imagen}" alt="${obj.modelo}">
+                <h3 class="font-medium pl-4 text-lg">${obj.marca}</h3>
+                <h3 class="font-medium pl-4 text-lg">${obj.modelo}</h3>
+                <p class="pl-4 pb-[10px]">${obj.descripcion.slice(0, 40)}...</p>
+                <p class="pl-4 pb-[10px] font-bold">$${precioMostrar}</p>
+                <div class='flex flex-row justify-around'>
+                    <a href="./detalles.html?id=${obj.id}" class="bg-black text-white p-2 w-2/4 text-center flex justify-center items-center rounded-xl">Detalles</a>
+                    <button>
+                        <img class='w-[50px]' src="" alt="">
+                    </button>
+                    ${!productoPush.includes(obj.id) ?
+                        `<button>
+                            <img class='w-[50px]' src="./assests/img/shopping-cart-verified-symbol_icon-icons.com_56123.png" alt="" data-compra ='${obj.id}'>
+                        </button>` :
+                        `<button>
+                            <img class='w-[50px]' src="./assests/img/shopping-cart-cancel-button_icon-icons.com_56130.png" alt="" data-compra ='${obj.id}'>
+                        </button>`
+                    }
+                </div>
+            </div>`;
         
-    };
-    const renderNoResultsMessage = (contenedor) => {
-        contenedor.innerHTML = "<h2 class='text-center'>No hay productos que coincidan con su búsqueda</h2>";
-    };
+        contenedor.innerHTML += template;
+    });
+}
+
+const renderNoResultsMessage = (contenedor) => {
+    contenedor.innerHTML = "<h2 class='text-center'>No hay productos que coincidan con su búsqueda</h2>";
+};
+
 
 //console.log( renderCards(data,contenedorCards));
 renderCards(data, contenedorCards)
